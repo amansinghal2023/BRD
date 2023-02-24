464c4e847fa20315b13c08f757d0aae1926d8565
Aman <Aman.s@antino.io>
2023-02-24 10:48:53 +0530

    SigninSingup

---
+++ Authentication/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Signup(models.Model):
+    username=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)
+    password=models.CharField(max_length=20)
+    c_password=models.CharField(max_length=20)
+
+
